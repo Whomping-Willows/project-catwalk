7530d449f92f68a4e0c88267e7d141a8b7123e56
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-18 17:17:37 -0600

    Minor jsx changes to ReviewsItem before making new branch

--- client/src/components/review/ReviewsItem.jsx
+++ client/src/components/review/ReviewsItem.jsx
@@ -2,7 +2,13 @@
 import React from 'react';
 
 const ReviewsItem = () => (
-  <div className="reviews" />
+  <div id="reviewItem">
+    <div id="reviewItemSubTitle">
+      <div id="reviewSubTitleStars">STARS GO HERE</div>
+      <div id="reviewSubTitleDate">MONTH DD, YYYY</div>
+    </div>
+    <h3 id="reviewTitle">The title goes here</h3>
+  </div>
 );
 
 export default ReviewsItem;
