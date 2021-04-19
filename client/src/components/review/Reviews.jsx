3fe3a5c5ba0ad2de89dacf4867e586d62b6acd43
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-18 18:02:46 -0600

    Created reviewsData in Reviews.jsx, exported to item and updating div's accordingly

--- client/src/components/review/Reviews.jsx
+++ client/src/components/review/Reviews.jsx
@@ -4,6 +4,74 @@ import ReviewsBreakdown from './ReviewsBreakdown.jsx';
 import ReviewsList from './ReviewsList.jsx';
 import ReviewsForm from './ReviewsForm.jsx';
 
+export const reviewsData = {
+  product: '18078',
+  page: 0,
+  count: 5,
+  results: [
+    {
+      review_id: 289045,
+      rating: 1,
+      summary: 'title',
+      recommend: true,
+      response: null,
+      body: 'body',
+      date: '2021-03-13T00:00:00.000Z',
+      reviewer_name: 'user',
+      helpfulness: 2,
+      photos: [],
+    },
+    {
+      review_id: 289043,
+      rating: 2,
+      summary: 'cd',
+      recommend: false,
+      response: null,
+      body: 'css',
+      date: '2021-03-13T00:00:00.000Z',
+      reviewer_name: 'Taylor Smart',
+      helpfulness: 0,
+      photos: [Array],
+    },
+    {
+      review_id: 289038,
+      rating: 2,
+      summary: 'a',
+      recommend: false,
+      response: null,
+      body: 'aaaaaa',
+      date: '2021-03-13T00:00:00.000Z',
+      reviewer_name: 'Taylor Smart',
+      helpfulness: 0,
+      photos: [Array],
+    },
+    {
+      review_id: 289034,
+      rating: 2,
+      summary: 'fffeee',
+      recommend: false,
+      response: null,
+      body: 'fffdsss',
+      date: '2021-03-13T00:00:00.000Z',
+      reviewer_name: 'Taylor Smart',
+      helpfulness: 0,
+      photos: [Array],
+    },
+    {
+      review_id: 289033,
+      rating: 2,
+      summary: 'fews',
+      recommend: false,
+      response: null,
+      body: 'fdsfes',
+      date: '2021-03-13T00:00:00.000Z',
+      reviewer_name: 'Taylor Smart',
+      helpfulness: 0,
+      photos: [Array],
+    },
+  ],
+};
+
 const Reviews = () => (
   <div className="reviews">
     <h2>RATINGS & REVIEWS</h2>
--- client/src/components/review/ReviewsItem.jsx
+++ client/src/components/review/ReviewsItem.jsx
@@ -1,13 +1,30 @@
-/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import React from 'react';
+import { reviewsData } from './Reviews.jsx';
+
+const formatDate = (string) => {
+  let formatted = '';
+  const year = string.substring(0, 4);
+  const month = string.substring(5, 7);
+  const day = string.substring(8, 10);
+
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const monthNumber = Number(month);
+  const monthString = monthNames[monthNumber - 1];
+
+  formatted = formatted.concat(monthString, ' ', day, ', ', year);
+
+  return formatted;
+};
 
 const ReviewsItem = () => (
   <div id="reviewItem">
     <div id="reviewItemSubTitle">
       <div id="reviewSubTitleStars">STARS GO HERE</div>
-      <div id="reviewSubTitleDate">MONTH DD, YYYY</div>
+      <div id="reviewSubTitleDate">{formatDate(reviewsData.results[2].date)}</div>
     </div>
-    <h3 id="reviewTitle">The title goes here</h3>
+    <h3 id="reviewTitle">{reviewsData.results[2].summary}</h3>
+    <p id="reviewBody">{reviewsData.results[2].body}</p>
   </div>
 );
 
