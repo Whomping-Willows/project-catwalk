a832118aa8abe65c9fe53170dc48fd942383ad3d
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-19 16:58:12 -0600

    Filter bars now formatted as expected

--- client/src/components/review/ReviewsFilterBlock.jsx
+++ client/src/components/review/ReviewsFilterBlock.jsx
@@ -4,26 +4,24 @@ import HSBar from 'react-horizontal-stacked-bar-chart';
 
 const ReviewsFilterBlock = () => (
   <div id="reviewsFilterBlock">
-    <div id="reviews5Stars">
-      <p id="reviews5Label">5 Stars</p>
-      <div id="reviews5Data">
-        <HSBar height={10} width={50} data={[{ value: 7, color: 'green' }, { value: 3, color: 'lightgrey' }]} className="reviewsBar" />
-      </div>
+    <p id="reviews5Label">5 Stars</p>
+    <div id="reviews5Data">
+      <HSBar height={10} data={[{ value: 7, color: 'green' }, { value: 3, color: 'lightgrey' }]} className="reviewsBar" />
     </div>
-    <div id="reviews4Stars">
-      <p id="reviews4Label">4 Stars</p>
+    <p id="reviews4Label">4 Stars</p>
+    <div id="reviews4Data">
       <HSBar height={10} data={[{ value: 9, color: 'green' }, { value: 1, color: 'lightgrey' }]} className="reviewsBar" id="reviews4Data" />
     </div>
-    <div id="reviews3Stars">
-      <p id="reviews3Label">3 Stars</p>
+    <p id="reviews3Label">3 Stars</p>
+    <div id="reviews3Data">
       <HSBar height={10} data={[{ value: 1, color: 'green' }, { value: 9, color: 'lightgrey' }]} className="reviewsBar" id="reviews3Data" />
     </div>
-    <div id="reviews2Stars">
-      <p id="reviews2Label">2 Stars</p>
+    <p id="reviews2Label">2 Stars</p>
+    <div id="reviews2Data">
       <HSBar height={10} data={[{ value: 0, color: 'green' }, { value: 10, color: 'lightgrey' }]} className="reviewsBar" id="reviews2Data" />
     </div>
-    <div id="reviews1Stars">
-      <p id="reviews1Label">1 Stars</p>
+    <p id="reviews1Label">1 Stars</p>
+    <div id="reviews1Data">
       <HSBar height={10} data={[{ value: 2, color: 'green' }, { value: 8, color: 'lightgrey' }]} className="reviewsBar" id="reviews1Data" />
     </div>
   </div>
--- client/src/components/review/ReviewsSummary.jsx
+++ client/src/components/review/ReviewsSummary.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 const ReviewsSummary = () => (
   <div id="reviewsSummary">
-    <span>
-      <div id="reviewsSumNum">3.5</div>
-      <div id="reviewsSumStars">[STARS GO HERE]</div>
-    </span>
-    <span>
-      <p id="reviewsSumRec">100% of reviews recommend this product</p>
-    </span>
+    <div id="reviewsSumNum">3.5</div>
+    <div id="reviewsSumStars">[STARS GO HERE]</div>
+    <div id="reviewsSumRec">
+      <p>100% of reviews recommend this product</p>
+    </div>
   </div>
 );
 
