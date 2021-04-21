6984fa718658cc36c0692a9b9fc96b3bb3a77a9c
Ben Lambert <kalstarn@gmail.com>
2021-04-21 12:01:45 -0600

    resync with test branch

--- client/src/components/product/Product.jsx
+++ client/src/components/product/Product.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 
 const Product = () => (
   <div className="product">
-    <div id="productGallery" />
-    <div id="productStyles" />
-    <div id="productDescription" />
+    <div id="productGallery"></div>
+    <div id="productStars"></div>
+    <div id="productName"></div>
+    <div id="productStyles"></div>
+    <div id="productCart"></div>
+    <div id="productDescription"></div>
   </div>
 );
 
