feb00bd6b8f10ca64ec758c4a0d94e4a2a0463e9
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-21 16:29:03 -0600

    More reviews button successfully renders two new reviews at a time

--- client/src/components/review/ReviewsList.jsx
+++ client/src/components/review/ReviewsList.jsx
@@ -1,26 +1,39 @@
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useState } from 'react';
 import ReviewsSort from './ReviewsSort.jsx';
 import ReviewsItem from './ReviewsItem.jsx';
 
-const ReviewsList = (props) => (
-  <div id="reviewsList">
-    <ReviewsSort reviewsMetaData={props.reviewsMetaData} />
-    <div id="reviewsAll">
-      {props.reviewsProductData.results.map((review) => (
-        <ReviewsItem key={review.review_id} review={review} />
-      ))}
+const ReviewsList = (props) => {
+  const [reviewsInList, setReviewsInList] = useState(props.reviewsProductData.results.slice(0, 2));
+  const [index, setIndex] = useState(4);
+
+  const addTwo = () => {
+    console.log('button clicked');
+    const newList = props.reviewsProductData.results.slice(0, index);
+    console.log('NewList ', newList);
+    setReviewsInList(newList);
+    setIndex(index + 2);
+  };
+
+  return (
+    <div id="reviewsList">
+      <ReviewsSort reviewsMetaData={props.reviewsMetaData} />
+      <div id="reviewsAll">
+        {reviewsInList.map((review) => (
+          <ReviewsItem key={review.review_id} review={review} />
+        ))}
+      </div>
+      <div id="reviewsListButtons">
+        <button type="button" id="reviewsLoadMore" onClick={addTwo}>MORE REVIEWS</button>
+        <button type="button" id="reviewsAdd">
+          ADD A REVIEW
+          <i className="fas fa-plus" id="reviewsAddPlus" />
+        </button>
+      </div>
     </div>
-    <div id="reviewsListButtons">
-      <button type="button" id="reviewsLoadMore">MORE REVIEWS</button>
-      <button type="button" id="reviewsAdd">
-        ADD A REVIEW
-        <i className="fas fa-plus" id="reviewsAddPlus" />
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ReviewsList;
