d341f0ebcccd8aae1eddd9a85f7897c18c1c081c
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-22 16:46:05 -0600

    Added first two http tests to test supertest

---
+++ __tests__/app.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+
+describe("App-wide API tests", () => {
+
+  it("should get the list of products", async () => {
+    await getRequest(end.listProducts)
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+test('Jest setup test', () => {
+  expect(1).toBe(1);
+})
---
+++ __tests__/supertest.js
@@ -0,0 +1,42 @@
+const request = require('supertest');
+const express = require('express');
+const axios = require('axios');
+const GITHUB_API_KEY = require('../server/config.js');
+
+const router = express.Router();
+const app = express();
+app.use(express.json());
+app.use(express.static('./client/dist'));
+app.use('/', router);
+
+const noData = {};
+
+router.get('/api', (req, res) => {
+  axios.get(req.headers.endpoint, {
+    headers: { Authorization: GITHUB_API_KEY },
+  })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+    });
+});
+
+router.put('/api', (req, res) => {
+  axios.put(req.body.headers.endpoint, noData, {
+    headers: { Authorization: GITHUB_API_KEY },
+  })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((err) => {
+    });
+});
+
+request(app)
+  .get('/api')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .end((err, res) => {
+    if (err) throw err;
+  });
