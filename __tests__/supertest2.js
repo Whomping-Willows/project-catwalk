8fde0f997c81ed1c121ab4f04d01a7e95688f4da
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-23 14:27:42 -0600

    Edits with Truman OH

---
+++ __tests__/supertest2.js
@@ -0,0 +1,38 @@
+import 'regenerator-runtime/runtime';
+
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ name: 'john' });
+});
+
+app.put('/api', (req, res) => {
+  res.status(200).json({ name: 'john' });
+});
+
+describe('App-wide server tests', () => {
+  it('should performa a get request', () => {
+    request(app)
+      .get('/api')
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '15')
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+      });
+  });
+
+  it('should performa a put request', () => {
+    request(app)
+      .put('/api')
+      .expect('Content-Type', /json/)
+      .expect('Content-Length', '15')
+      .expect(200)
+      .end((err, res) => {
+        if (err) throw err;
+      });
+  });
+});
