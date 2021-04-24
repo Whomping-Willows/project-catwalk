6eb6a17730625131ca8e98a5553fbff94709b2b2
Ben Lambert <kalstarn@gmail.com>
2021-04-23 18:46:43 -0600

    basic structure complete plus some styling and rendering for style selection

--- client/src/components/product/Product.jsx
+++ client/src/components/product/Product.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
 /* eslint-disable import/extensions */
 import React, { useState, useContext, useEffect } from 'react';
 import { ApiContext } from '../../contexts/api.context.jsx';
@@ -8,26 +10,15 @@ import ProductStars from './productStars.jsx';
 import ProductStyles from './productStyles.jsx';
 import ProductCart from './productCart.jsx';
 
-const Product = () => {
-  const [productProductData, setProductProductData] = useState();
-  const [productStyleData, setProductStyleData] = useState();
-  const { end, getRequest } = useContext(ApiContext);
-
-  useEffect(() => {
-    getRequest(end.productStyles, setProductStyleData);
-    getRequest(end.listInfo, setProductProductData);
-  }, []);
-
-  return (
-    <div className="product">
-      <ProductGallery />
-      <ProductStars />
-      <ProductName />
-      <ProductStyles />
-      <ProductCart />
-      <ProductDescription />
-    </div>
-  );
-};
+const Product = (props) => (
+  <div className="product">
+    <ProductGallery />
+    <ProductStars />
+    <ProductName product={props.currentProduct} />
+    <ProductStyles styles={props.currentStyles} />
+    <ProductCart />
+    <ProductDescription />
+  </div>
+);
 
 export default Product;
