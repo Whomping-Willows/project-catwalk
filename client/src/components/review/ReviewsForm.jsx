cff34f5571c3f129134c5addb43ce9019ea9a028
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-25 13:03:58 -0600

    Fixed chars in form to dynamically display description as requested

--- client/src/components/review/ReviewsForm.jsx
+++ client/src/components/review/ReviewsForm.jsx
@@ -5,14 +5,82 @@
 /* eslint-disable guard-for-in */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import React, { useContext, Fragment } from 'react';
+import React, { useContext, useState } from 'react';
 import { ApiContext } from '../../contexts/api.context.jsx';
 import StarsReviewForm from './StarsReviewForm.jsx';
 
 const ReviewsForm = (props) => {
   const { productName } = useContext(ApiContext);
+  const [sizeDescription, setSizeDescription] = useState('none selected');
+  const [widthDescription, setWidthDescription] = useState('none selected');
+  const [comfortDescription, setComfortDescription] = useState('none selected');
+  const [qualityDescription, setQualityDescription] = useState('none selected');
+  const [lengthDescription, setLengthDescription] = useState('none selected');
+  const [fitDescription, setFitDescription] = useState('none selected');
 
   const chars = props.reviewsMetaData.characteristics;
+  const charsDescriptions = {
+    size: {
+      1: 'A size too small',
+      2: '1/2 size too small',
+      3: 'Perfect',
+      4: '1/2 size too big',
+      5: 'A size too big',
+    },
+    width: {
+      1: 'Too narrow',
+      2: 'Slightly narrow',
+      3: 'Perfect',
+      4: 'Slightly wide',
+      5: 'Too wide',
+    },
+    comfort: {
+      1: 'Uncomfortable',
+      2: 'Slightly uncomfortable',
+      3: 'Ok',
+      4: 'Comfortable',
+      5: 'Perfect',
+    },
+    quality: {
+      1: 'Poor',
+      2: 'Below average',
+      3: 'What I expected',
+      4: 'Pretty great',
+      5: 'Perfect',
+    },
+    length: {
+      1: 'Runs short',
+      2: 'Slightly short',
+      3: 'Perfect',
+      4: 'Slightly long',
+      5: 'Runs long',
+    },
+    fit: {
+      1: 'Runs tight',
+      2: 'Slightly tight',
+      3: 'Perfect',
+      4: 'Slightly loose',
+      5: 'Runs loose',
+    },
+  };
+
+  const setDescription = (e) => {
+    const charName = e.target.name;
+    const charValue = Number(e.target.value);
+    if (charName === 'size') {
+      setSizeDescription(charsDescriptions[charName][charValue]);
+    } else if (charName === 'width') {
+      setWidthDescription(charsDescriptions[charName][charValue]);
+    } else if (charName === 'comfort') {
+      setComfortDescription(charsDescriptions[charName][charValue]);
+    } else if (charName === 'quality') {
+      setQualityDescription(charsDescriptions[charName][charValue]);
+    } else if (charName === 'length') {
+      setLengthDescription(charsDescriptions[charName][charValue]);
+    } else if (charName === 'fit') {
+      setFitDescription(charsDescriptions[charName][charValue]);
+    }
+  };
 
   return (
 
@@ -24,173 +92,158 @@ const ReviewsForm = (props) => {
           {` ${productName}`}
         </h3>
       </div>
-      <div id="reviewsOverallRatingDiv">
+      <div className="reviewsFormBorder" id="reviewsOverallRatingDiv">
         <h3 className="reviewsFormLabelsMain" id="overallRatingStars">Overall Rating</h3>
         <StarsReviewForm id="overallRatingStars" />
       </div>
-      <div id="reviewsFormRecDiv">
+      <div className="reviewsFormBorder" id="reviewsFormRecDiv">
         <>
           <h3 className="reviewsFormLabelsMain">Do you recommend this product?</h3>
         </>
         <label htmlFor="ReviewsYes" className="reviewsFormLabelsSm">Yes</label>
-        <input type="radio" id="ReviewsYes" value="Yes" />
-        <label htmlFor="ReviewsNo" className="reviewsFormLabelsSm">Yes</label>
-        <input type="radio" id="ReviewsNo" value="No" />
+        <input type="radio" id="ReviewsYes" value="Yes" name="recommend" />
+        <label htmlFor="ReviewsNo" className="reviewsFormLabelsSm">No</label>
+        <input type="radio" id="ReviewsNo" value="No" name="recommend" />
       </div>
-<div id="reviewsFormCharTitle">Please rate the following factors of this product based on your experience:</div>
+      <div className="reviewsFormBorder" id="reviewsFormCharTitle">Please rate the following factors of this product based on your experience:</div>
       <div id="reviewsFormCharDiv">
+
         {chars.Size && (
           <div className="charGrid">
             <h3 className="reviewsFormLabelsMain charTitle">Size</h3>
-            <label htmlFor="Size1" className="reviewsFormLabelsSm char1Title">A size too small</label>
+            <p className="reviewsFormLabelsSm charDescription">{sizeDescription}</p>
 
-            <input type="radio" id="Size1" className="char1Radio" value="1" />
-            <label htmlFor="Size1" className="reviewsFormLabelsSm char2Title">1/2 size too small</label>
+            <input type="radio" id="Size1" className="char1Radio" value="1" name="size" onClick={setDescription} />
+            <label htmlFor="Size1" className="reviewsFormLabelsSm char1Score">1</label>
 
-            <input type="radio" id="Size2" className="char2Radio" value="2" />
-            <label htmlFor="Size3" className="reviewsFormLabelsSm char3Title" id="Size3">Perfect</label>
+            <input type="radio" id="Size2" className="char2Radio" value="2" name="size" onClick={setDescription} />
+            <label htmlFor="Size2" className="reviewsFormLabelsSm char2Title" id="Size2" />
 
-            <input type="radio" id="Size3" className="char3Radio" value="3" />
-            <label htmlFor="Size4" className="reviewsFormLabelsSm char4Title" id="size4">1/2 size too big</label>
+            <input type="radio" id="Size3" className="char3Radio" value="3" name="size" onClick={setDescription} />
+            <label htmlFor="Size3" className="reviewsFormLabelsSm char3Title" id="size3" />
 
-            <input type="radio" id="Size4" className="char4Radio" value="4" />
-            <label htmlFor="Size5" className="reviewsFormLabelsSm char5Title">A size too big</label>
+            <input type="radio" id="Size4" className="char4Radio" value="4" name="size" onClick={setDescription} />
+            <label htmlFor="Size4" className="reviewsFormLabelsSm char4Title" />
 
-            <input type="radio" id="Size5" className="char5Radio" value="5" />
+            <input type="radio" id="Size5" className="char5Radio" value="5" name="size" onClick={setDescription} />
+            <label htmlFor="Size1" className="reviewsFormLabelsSm char5Score">5</label>
           </div>
         )}
 
         {chars.Width && (
           <div className="charGrid">
             <h3 className="reviewsFormLabelsMain charTitle">Width</h3>
-            <label htmlFor="Width1" className="reviewsFormLabelsSm char1Title">
-              Too narrow
-            </label>
-            <input type="radio" id="Width1" className="char1Radio" value="1" />
-            <label htmlFor="Width1" className="reviewsFormLabelsSm char2Title">
-              Slightly narrow
-            </label>
-            <input type="radio" id="Width2" className="char2Radio" value="2" />
-            <label htmlFor="Width3" className="reviewsFormLabelsSm char3Title" id="Width3">
-              Perfect
-            </label>
-            <input type="radio" id="Width3" className="char3Radio" value="3" />
-            <label htmlFor="Width4" className="reviewsFormLabelsSm char4Title" id="Width4">
-              Slightly wide
-            </label>
-            <input type="radio" id="Width4" className="char4Radio" value="4" />
-            <label htmlFor="Width5" className="reviewsFormLabelsSm char5Title">
-              Too wide
-            </label>
-            <input type="radio" id="Width5" className="char5Radio" value="5" />
+            <p className="reviewsFormLabelsSm charDescription">{widthDescription}</p>
+
+            <input type="radio" id="Width1" className="char1Radio" value="1" name="width" onClick={setDescription} />
+            <label htmlFor="Width1" className="reviewsFormLabelsSm char1Score">1</label>
+
+            <input type="radio" id="Width2" className="char2Radio" value="2" name="width" onClick={setDescription} />
+            <label htmlFor="Width2" className="reviewsFormLabelsSm char2Title" />
+
+            <input type="radio" id="Width3" className="char3Radio" value="3" name="width" onClick={setDescription} />
+            <label htmlFor="Width3" className="reviewsFormLabelsSm char3Title" id="Width3" />
+
+            <input type="radio" id="Width4" className="char4Radio" value="4" name="width" onClick={setDescription} />
+            <label htmlFor="Width4" className="reviewsFormLabelsSm char4Title" id="Width4" />
+
+            <input type="radio" id="Width5" className="char5Radio" value="5" name="width" onClick={setDescription} />
+            <label htmlFor="Width5" className="reviewsFormLabelsSm char5Score"> 5</label>
           </div>
         )}
 
         {chars.Comfort && (
           <div className="charGrid">
             <h3 className="reviewsFormLabelsMain charTitle">Comfort</h3>
-            <label htmlFor="Comfort1" className="reviewsFormLabelsSm char1Title">
-              Uncomfortable
-            </label>
-            <input type="radio" id="Comfort1" className="char1Radio" value="1" />
-            <label htmlFor="Comfort1" className="reviewsFormLabelsSm char2Title">
-              Slightly uncomfortable
-            </label>
-            <input type="radio" id="Comfort2" className="char2Radio" value="2" />
-            <label htmlFor="Comfort3" className="reviewsFormLabelsSm char3Title" id="Comfort3">
-              Ok
-            </label>
-            <input type="radio" id="Comfort3" className="char3Radio" value="3" />
-            <label htmlFor="Comfort4" className="reviewsFormLabelsSm char4Title" id="Comfort4">
-              Comfortable
-            </label>
-            <input type="radio" id="Comfort4" className="char4Radio" value="4" />
-            <label htmlFor="Comfort5" className="reviewsFormLabelsSm char5Title">
-              Perfect
-            </label>
-            <input type="radio" id="Comfort5" className="char5Radio" value="5" />
+            <p className="reviewsFormLabelsSm charDescription">{comfortDescription}</p>
+
+            <input type="radio" id="Comfort1" className="char1Radio" value="1" name="comfort" onClick={setDescription} />
+            <label htmlFor="Comfort1" className="reviewsFormLabelsSm char1Score">1</label>
+
+            <input type="radio" id="Comfort2" className="char2Radio" value="2" name="comfort" onClick={setDescription} />
+            <label htmlFor="Comfort1" className="reviewsFormLabelsSm char2Title" />
+
+            <input type="radio" id="Comfort3" className="char3Radio" value="3" name="comfort" onClick={setDescription} />
+            <label htmlFor="Comfort3" className="reviewsFormLabelsSm char3Title" id="Comfort3" />
+
+            <input type="radio" id="Comfort4" className="char4Radio" value="4" name="comfort" onClick={setDescription} />
+            <label htmlFor="Comfort4" className="reviewsFormLabelsSm char4Title" id="Comfort4" />
+
+            <input type="radio" id="Comfort5" className="char5Radio" value="5" name="comfort" onClick={setDescription} />
+            <label htmlFor="Comfort5" className="reviewsFormLabelsSm char5Score">5</label>
           </div>
         )}
 
         {chars.Quality && (
           <div className="charGrid">
             <h3 className="reviewsFormLabelsMain charTitle">Quality</h3>
-            <label htmlFor="Quality1" className="reviewsFormLabelsSm char1Title">
-              Poor
-            </label>
-            <input type="radio" id="Quality1" className="char1Radio" value="1" />
-            <label htmlFor="Quality1" className="reviewsFormLabelsSm char2Title">
-              Below average
-            </label>
-            <input type="radio" id="Quality2" className="char2Radio" value="2" />
-            <label htmlFor="Quality3" className="reviewsFormLabelsSm char3Title" id="Quality3">
-              What I expected
-            </label>
-            <input type="radio" id="Quality3" className="char3Radio" value="3" />
-            <label htmlFor="Quality4" className="reviewsFormLabelsSm char4Title" id="Quality4">
-              Pretty great
-            </label>
-            <input type="radio" id="Quality4" className="char4Radio" value="4" />
-            <label htmlFor="Quality5" className="reviewsFormLabelsSm char5Title">
-              Perfect
-            </label>
-            <input type="radio" id="Quality5" className="char5Radio" value="5" />
+            <p className="reviewsFormLabelsSm charDescription">{qualityDescription}</p>
+
+            <input type="radio" id="Quality1" className="char1Radio" value="1" name="quality" onClick={setDescription} />
+            <label htmlFor="Quality1" className="reviewsFormLabelsSm char1Score">1</label>
+
+            <input type="radio" id="Quality2" className="char2Radio" value="2" name="quality" onClick={setDescription} />
+            <label htmlFor="Quality1" className="reviewsFormLabelsSm char2Title" />
+
+            <input type="radio" id="Quality3" className="char3Radio" value="3" name="quality" onClick={setDescription} />
+            <label htmlFor="Quality3" className="reviewsFormLabelsSm char3Title" id="Quality3" />
+
+            <input type="radio" id="Quality4" className="char4Radio" value="4" name="quality" onClick={setDescription} />
+            <label htmlFor="Quality4" className="reviewsFormLabelsSm char4Title" id="Quality4" />
+
+            <input type="radio" id="Quality5" className="char5Radio" value="5" name="quality" onClick={setDescription} />
+            <label htmlFor="Quality5" className="reviewsFormLabelsSm char5Score">5</label>
           </div>
         )}
 
         {chars.Length && (
           <div className="charGrid">
             <h3 className="reviewsFormLabelsMain charTitle">Length</h3>
-            <label htmlFor="Length1" className="reviewsFormLabelsSm char1Title">
-              Runs short
-            </label>
-            <input type="radio" id="Length1" className="char1Radio" value="1" />
-            <label htmlFor="Length1" className="reviewsFormLabelsSm char2Title">
-              Runs slightly short
-            </label>
-            <input type="radio" id="Length2" className="char2Radio" value="2" />
-            <label htmlFor="Length3" className="reviewsFormLabelsSm char3Title" id="Length3">
-              Perfect
-            </label>
-            <input type="radio" id="Length3" className="char3Radio" value="3" />
-            <label htmlFor="Length4" className="reviewsFormLabelsSm char4Title" id="Length4">
-              Runs slightly long
-            </label>
-            <input type="radio" id="Length4" className="char4Radio" value="4" />
-            <label htmlFor="Length5" className="reviewsFormLabelsSm char5Title">
-              Runs long
-            </label>
-            <input type="radio" id="Length5" className="char5Radio" value="5" />
+            <p className="reviewsFormLabelsSm charDescription">{lengthDescription}</p>
+
+            <input type="radio" id="Length1" className="char1Radio" value="1" name="length" onClick={setDescription} />
+            <label htmlFor="Length1" className="reviewsFormLabelsSm char1Score">1</label>
+
+            <input type="radio" id="Length2" className="char2Radio" value="2" name="length" onClick={setDescription} />
+            <label htmlFor="Length1" className="reviewsFormLabelsSm char2Title" />
+
+            <input type="radio" id="Length3" className="char3Radio" value="3" name="length" onClick={setDescription} />
+            <label htmlFor="Length3" className="reviewsFormLabelsSm char3Title" id="Length3" />
+
+            <input type="radio" id="Length4" className="char4Radio" value="4" name="length" onClick={setDescription} />
+            <label htmlFor="Length4" className="reviewsFormLabelsSm char4Title" id="Length4" />
+
+            <input type="radio" id="Length5" className="char5Radio" value="5" name="length" onClick={setDescription} />
+            <label htmlFor="Length5" className="reviewsFormLabelsSm char5Score">5</label>
           </div>
         )}
 
         {chars.Fit && (
           <div className="charGrid">
             <h3 className="reviewsFormLabelsMain charTitle">Fit</h3>
-            <label htmlFor="Fit1" className="reviewsFormLabelsSm char1Title">
-              Runs tight
-            </label>
-            <input type="radio" id="Fit1" className="char1Radio" value="1" />
-            <label htmlFor="Fit1" className="reviewsFormLabelsSm char2Title">
-              Runs slightly tight
-            </label>
-            <input type="radio" id="Fit2" className="char2Radio" value="2" />
-            <label htmlFor="Fit3" className="reviewsFormLabelsSm char3Title" id="Fit3">
-              Perfect
-            </label>
-            <input type="radio" id="Fit3" className="char3Radio" value="3" />
-            <label htmlFor="Fit4" className="reviewsFormLabelsSm char4Title" id="Fit4">
-              Runs slightly loose
-            </label>
-            <input type="radio" id="Fit4" className="char4Radio" value="4" />
-            <label htmlFor="Fit5" className="reviewsFormLabelsSm char5Title">
-              Runs loose
-            </label>
-            <input type="radio" id="Fit5" className="char5Radio" value="5" />
+            <p className="reviewsFormLabelsSm charDescription">{fitDescription}</p>
+
+            <input type="radio" id="Fit1" className="char1Radio" value="1" name="fit" onClick={setDescription} />
+            <label htmlFor="Fit1" className="reviewsFormLabelsSm char1Score">1</label>
+
+            <input type="radio" id="Fit2" className="char2Radio" value="2" name="fit" onClick={setDescription} />
+            <label htmlFor="Fit1" className="reviewsFormLabelsSm char2Title" />
+
+            <input type="radio" id="Fit3" className="char3Radio" value="3" name="fit" onClick={setDescription} />
+            <label htmlFor="Fit3" className="reviewsFormLabelsSm char3Title" id="Fit3" />
+
+            <input type="radio" id="Fit4" className="char4Radio" value="4" name="fit" onClick={setDescription} />
+            <label htmlFor="Fit4" className="reviewsFormLabelsSm char4Title" id="Fit4" />
+
+            <input type="radio" id="Fit5" className="char5Radio" value="5" name="fit" onClick={setDescription} />
+            <label htmlFor="Fit5" className="reviewsFormLabelsSm char5Score">5</label>
           </div>
         )}
       </div>
-
+      <div className="reviewsFormBorder" id="reviewsFormSum">
+        <label htmlFor="summary" className="reviewsFormLabelsSm" />
+        <input type="text" className="reviewsFormInput" id="summary" />
+      </div>
     </form>
 
   );
