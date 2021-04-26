d8b8b470974a855b16a7444f7b2d51b4a401440c
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-26 15:18:39 -0600

    Fixed sort dropdown menu bug

--- client/src/components/product/Product.jsx
+++ client/src/components/product/Product.jsx
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
-/* eslint-disable no-unused-vars */
-=======
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
->>>>>>> 2a246245d170cdb01153a6fac33177b8771d1b3f
 /* eslint-disable import/extensions */
-import React, { useState, useContext, useEffect } from 'react';
-import { ApiContext } from '../../contexts/api.context.jsx';
+import React, { useState } from 'react';
+// import { ApiContext } from '../../contexts/api.context.jsx';
 import ProductDescription from './productDescription.jsx';
 import ProductGallery from './productGallery.jsx';
 import ProductName from './productName.jsx';
@@ -17,16 +13,9 @@ import ProductCart from './productCart.jsx';
 const Product = (props) => {
   const [selectedStyle, setSelectedStyle] = useState(props.currentStyles.results[0].style_id);
 
-<<<<<<< HEAD
-  useEffect(() => {
-    getRequest(end.productStyles, setProductStyleData);
-    getRequest(end.listInfo, setProductProductData);
-  }, []);
-=======
   // useEffect(() => {
   //   setSelectedStyle(props.currentStyles.results[0].style_id);
   // });
->>>>>>> 2a246245d170cdb01153a6fac33177b8771d1b3f
 
   return (
     <div className="product">
