034afe7e9d55a9342a707f7c600a39454dc4f196
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-26 17:23:12 -0600

    Successfully got one related product to render on page

--- client/src/components/related/Related.jsx
+++ client/src/components/related/Related.jsx
@@ -2,32 +2,85 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/extensions */
 import React, { useState, useEffect, useContext } from 'react';
-import RelatedProductsList from './relatedProductsList.jsx';
-import YourOutFitList from './yourOutFitList.jsx';
 import { ApiContext } from '../../contexts/api.context.jsx';
 
 const Related = () => {
-  const [RelatedProductsId, setRelatedProductsId] = useState([]);
+  const [relatedProductsIds, setRelatedProductsIds] = useState([]);
+  const [allRelated, setAllRelated] = useState([]);
+  const [allRelatedStyles, setAllRelatedStyles] = useState([]);
 
   const { end, getRequest } = useContext(ApiContext);
 
-  useEffect(() => {
-    getRequest(end.relatedProducts, setRelatedProductsId);
-  }, []);
+  const addToRelated = (data) => {
+    const oldData = allRelated.slice();
+    oldData.push(data);
+    setAllRelated(oldData);
+  };
 
-  console.log('this is related data', RelatedProductsId);
+  const addToRelatedStyles = (data) => {
+    const oldStyles = allRelatedStyles.slice();
+    oldStyles.push(data);
+    setAllRelatedStyles(oldStyles);
+  };
+
+  const relatedCallback = (data) => {
+    setRelatedProductsIds(data);
+    data.forEach((id) => {
+      getRequest(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${id}`, addToRelated);
+      getRequest(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/products/${id}/styles`, addToRelatedStyles);
+    });
+  };
+
+  useEffect(() => {
+    getRequest(end.relatedProducts, relatedCallback);
+  }, []);
 
   return (
     <div className="related">
-      <h2 id="relatedProductsTitle">RELATED PRODUCTS</h2>
-      <div id="relatedProducts">
-        <RelatedProductsList RelatedProductsId={RelatedProductsId} />
-      </div>
-
-      <h2 id="relatedOutFitTitle">RELATED OUTFITS</h2>
-      <div id="relatedOutfit">
-        <YourOutFitList />
-      </div>
+      {relatedProductsIds && (
+        <>
+          <h2 id="relatedProductsTitle">RELATED PRODUCTS</h2>
+          <div id="relatedProducts">
+            {allRelated[0] && (
+              <div id="related1">
+                <img id="relatedPic1" src={allRelatedStyles[0].results[0].photos[0].thumbnail_url
+} />
+                <div id="relatedInfo1">
+                  <h3 className="relatedInfo">{allRelated[0].name}</h3>
+                  <p className="relatedInfo">{`$${allRelated[0].default_price}`}</p>
+                </div>
+              </div>
+            )}
+            {allRelated[1] && (
+              <div id="related2">
+                <img id="relatedPic2" src="#" />
+                <div id="relatedInfo2">
+                  <h3 className="relatedInfo">{allRelated[1].name}</h3>
+                  <p className="relatedInfo">{`$${allRelated[1].default_price}`}</p>
+                </div>
+              </div>
+            )}
+            {allRelated[2] && (
+              <div id="related3">
+                <img id="relatedPic3" src="#" />
+                <div id="relatedInfo3">
+                  <h3 className="relatedInfo">{allRelated[2].name}</h3>
+                  <p className="relatedInfo">{`$${allRelated[2].default_price}`}</p>
+                </div>
+              </div>
+            )}
+            {allRelated[3] && (
+              <div id="related4">
+                <img id="relatedPic4" src="#" />
+                <div id="relatedInfo4">
+                  <h3 className="relatedInfo">{allRelated[3].name}</h3>
+                  <p className="relatedInfo">{`$${allRelated[3].default_price}`}</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
