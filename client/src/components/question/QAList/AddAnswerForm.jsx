ed75747564099215d0bf14541e3ad190ca94840c
a-clowes <clowes.alison@gmail.com>
2021-04-26 19:19:54 -0600

    Add Answer button and modal added.

---
+++ client/src/components/question/QAList/AddAnswerForm.jsx
@@ -0,0 +1,91 @@
+/* eslint-disable import/extensions */
+import React, { useContext, useState } from 'react';
+import { ApiContext } from '../../../contexts/api.context.jsx';
+
+const AddAnswerForm = ({ handleClose }) => {
+  const {
+    productName, productId, postRequest, end,
+  } = useContext(ApiContext);
+
+  const [body, setQuestionBody] = useState();
+  const [name, setAskerName] = useState();
+  const [email, setEmail] = useState();
+
+  const postData = {
+    body,
+    name,
+    email,
+    product_id: productId,
+  };
+
+  const handleAskQuestionSubmit = (e) => {
+    e.preventDefault();
+    postRequest(end.addQuestion, postData);
+    // need a promise and alert that it has been submitted.
+
+    handleClose();
+  };
+
+  return (
+    <div id="questionFormContainer">
+      <h2>
+        Ask Your Question About
+        {` '${productName}' ` }
+        Here
+      </h2>
+      <form>
+        <label
+          htmlFor="questionBody"
+        >
+          <p>Your Question: (1000 characters max) </p>
+        </label>
+        <input
+          id="questionBody"
+          name="body"
+          type="text"
+          value={body}
+          onChange={(e) => setQuestionBody(e.target.value)}
+          maxLength="1000"
+          required
+        />
+        <label
+          htmlFor="askerName"
+        >
+          <p>Nickname: &quot;Example:jackson11!&quot;</p>
+        </label>
+        <input
+          id="askerName"
+          name="name"
+          type="text"
+          value={name}
+          onChange={(e) => { setAskerName(e.target.value); }}
+          maxLength="60"
+          required
+        />
+        <label
+          htmlFor="questioEmail"
+        >
+          <p>Email:</p>
+        </label>
+        <input
+          id="questionEmail"
+          name="email"
+          type="text"
+          value={email}
+          onChange={(e) => { setEmail(e.target.value); }}
+          maxLength="60"
+          required
+        />
+        <h3>For authentication reasons, you will not be emailed</h3>
+        <button
+          type="submit"
+          onClick={handleAskQuestionSubmit}
+        >
+          SUBMIT
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddAnswerForm;
--- client/src/components/question/QAList/QAListItem.jsx
+++ client/src/components/question/QAList/QAListItem.jsx
@@ -1,13 +1,40 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable react/prop-types */
 /* eslint-disable import/extensions */
 import React, { useState, useContext, useEffect } from 'react';
+import { Modal } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { ApiContext } from '../../../contexts/api.context.jsx';
 import AnswerList from './AnswerList.jsx';
+import AddAnswerForm from './AddAnswerForm.jsx';
 
 const QAListItem = ({ question }) => {
   const {
     getRequest, putRequest, setQuestionId, end,
   } = useContext(ApiContext);
+
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const useStyles = makeStyles({
+    askQuestionModal: {
+      position: 'absolute',
+      top: '10%',
+      left: '10%',
+      overflow: 'scroll',
+      height: '100%',
+      display: 'block',
+    },
+  });
+
+  const classes = useStyles();
 
   const setQuestionPromise = () => new Promise((resolve, reject) => {
     if (question.question_id) {
@@ -53,6 +80,31 @@ const QAListItem = ({ question }) => {
     margin: 10,
   };
 
+  const addAnswer = (
+    <div className="questionHelpful">
+      <button
+        className="helpfulButton"
+        id="addAnswerButton"
+        type="submit"
+        onClick={handleOpen}
+      >
+        Add Answer
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        container={() => document.getElementById('question')}
+        className={classes.askQuestionModal}
+      >
+        <AddAnswerForm
+          handleClose={handleClose}
+        />
+      </Modal>
+    </div>
+  );
+
   return (
     <li
       className="QAListItem"
@@ -62,6 +114,7 @@ const QAListItem = ({ question }) => {
         {` ${question.question_body}`}
       </h3>
       {helpful}
+      {addAnswer}
       <AnswerList
         answers={question.answers}
       />
