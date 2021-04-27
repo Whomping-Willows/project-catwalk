a14d43a1231676012729dc67e8e161cb54b56b58
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-26 21:21:19 -0600

    Two working reviews tests, still no working supertests

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,5 +1,38 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/user', function(req, res) {
+  res.status(200).json({ name: 'john' });
+});
+
+request(app)
+  .get('/user')
+  .expect('Content-Type', /json/)
+  .expect('Content-Length', '15')
+  .expect(200)
+  .end(function(err, res) {
+    if (err) throw err;
+  });
+
+  request(app)
+  .post('/users')
+  .send({name: 'john'})
+  .set('Accept', 'application/json')
+  .expect('Content-Type', /json/)
+  .expect(200)
+  .end(function(err, res) {
+    if (err) return done(err);
+    return done();
+
+/*
+////////////////////////////////////////////////////////////////
+//OUR ATTEMPT AT TESTING OUR OWN SERVER ////////////////////////
+////////////////////////////////////////////////////////////////
+
 import 'regenerator-runtime/runtime';
 
 const request = require('supertest');
@@ -9,36 +42,27 @@ const getEndpoint = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/product
 const putEndpoint = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-bld/reviews/289038/helpful';
 
 describe('App-wide server tests', () => {
-  it('should performa a get request', (done) => {
-    request(app)
-      .get('/api')
-      .send({
+  it('should performa a get request', async (done) => {
+    await request(app)
+      .get('/api', {
         headers: {
           endpoint: getEndpoint,
         },
       })
-      .expect((res) => {
-        res.data.product_id = '18078';
-      })
       .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        return done();
+      .then(async (response) => {
+        expect(res.data.product_id).toBe('18078');
       });
   });
 
-  it('should performa a put request', (done) => {
-    request(app)
-      .put('/api')
-      .send({
+  it('should performa a put request', async (done) => {
+    await request(app)
+      .put('/api', {
         headers: {
           endpoint: putEndpoint,
         },
       })
-      .expect(200)
-      .end((err, res) => {
-        if (err) return done(err);
-        return done();
-      });
+      .expect(200, done);
   });
 });
+*/
