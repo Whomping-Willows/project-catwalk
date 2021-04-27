3794d0fcd0f102abe839a5a284c349bf8b811855
Ben Lambert <kalstarn@gmail.com>
2021-04-27 09:51:44 -0600

    pre merge commit

--- client/src/components/question/QAList/AnswerList.jsx
+++ client/src/components/question/QAList/AnswerList.jsx
@@ -20,11 +20,10 @@ const AnswerList = ({ answers }) => {
 
   const answerList = rendered && (rendered.length > 0)
     ? rendered.map((answerId) => (
-      // <AnswerListItem
-      //   key={answerId}
-      //   answer={answers[answerId]}
-      // />
-      <></>
+      <AnswerListItem
+        key={answerId}
+        answer={answers[answerId]}
+      />
     ))
     : null;
 
