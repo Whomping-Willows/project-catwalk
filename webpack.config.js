d260a82c432c75f616f4caf17f2330262ef97501
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-28 14:18:54 -0600

    Added CompressionPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,22 @@
 const path = require('path');
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CompressionPlugin = require('compression-webpack-plugin');
+// const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: './client/src/index.js',
   output: {
-    filename: "bundle.js",
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'client', 'dist'),
   },
   plugins: [
-    new HTMLWebpackPlugin({
-      filename: "./index.html",
-      template: path.join(__dirname, 'public/index.html'),
-    }),
+    // new HTMLWebpackPlugin({
+    //   filename: './index.html',
+    //   template: path.join(__dirname, 'public/index.html'),
+    // }),
     new BundleAnalyzerPlugin(),
+    new CompressionPlugin(),
   ],
   module: {
     rules: [
@@ -22,11 +24,11 @@ module.exports = {
         test: /\.(js|jsx)?/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             presets: [
-              "@babel/preset-env",
-              "@babel/preset-react"
+              '@babel/preset-env',
+              '@babel/preset-react',
             ],
           },
         },
@@ -35,14 +37,14 @@ module.exports = {
         test: /\.html$/,
         use: [
           {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
-        ]
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.scss/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       // {
       //   test: /\.css$/,
