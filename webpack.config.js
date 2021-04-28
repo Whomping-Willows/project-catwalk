b83ed5780fb7cd31688b5d4f452d00461568bc6e
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-04-28 12:07:03 -0600

    Added plugins to optimize bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
 const path = require('path');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
+  mode: "production",
   entry: './client/src/index.js',
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, 'client', 'dist')
+    path: path.resolve(__dirname, 'client', 'dist'),
   },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: "./index.html",
+      template: path.join(__dirname, 'public/index.html'),
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
   module: {
     rules: [
       {
@@ -22,9 +32,22 @@ module.exports = {
         },
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
       },
+      {
+        test: /\.scss/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      // {
+      //   test: /\.css$/,
+      //   use: ['style-loader', 'css-loader'],
+      // },
     ],
   },
 };
