2982b9221cdfde83f4850f26ff3b93f958221739
a-clowes <clowes.alison@gmail.com>
2021-04-28 12:52:10 -0600

    fixed search bar styling.

--- client/src/components/question/SearchForm.jsx
+++ client/src/components/question/SearchForm.jsx
@@ -1,28 +1,53 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable import/extensions */
+import React, { useState, useContext, useEffect } from 'react';
+import { ApiContext } from '../../contexts/api.context.jsx';
 
-const SearchForm = () => (
-  <div id="searchContainer">
-    <form id="searchForm">
-      <label>
-        <span className="visually-hidden">Have a question? Search for Answers</span>
-      </label>
-      <input
-        id="searchInput"
-        type="text"
-        value=""
-        placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS"
-      />
-      <button
-        type="submit"
-        id="searchButton"
-      >
-        <i
-          className="fas fa-search"
-          id="searchAddIcon"
+const SearchForm = () => {
+  const { getRequest, end, productId } = useContext(ApiContext);
+  const [questionList, setQuestionList] = useState();
+  // const [loading, setLoading] = useState(true);
+  const [query, setQuery] = useState('');
+
+  useEffect(() => {
+    getRequest(end.listQuestions, setQuestionList).then(() => {
+      setLoading(false);
+    });
+  }, [productId]);
+
+  const handleSearchFormChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  // !loading ?
+  return (
+    <div
+     id="searchContainer"
+    >
+      <form id="searchForm">
+        <label className="visually-hidden">
+          Have a question? Search for Answers
+        </label>
+        <input
+          id="searchQuery"
+          type="text"
+          value={query}
+          onChange={handleSearchFormChange}
+          placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS"
         />
-      </button>
-    </form>
-  </div>
-);
+        <button
+          type="submit"
+          id="searchButton"
+        >
+          <i
+            className="fas fa-search"
+            id="searchAddIcon"
+          />
+        </button>
+      </form>
+    </div>
+  )
+  // : null;
+};
 
 export default SearchForm;
