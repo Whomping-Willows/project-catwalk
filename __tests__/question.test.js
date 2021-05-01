ef4b0a4804fe4311a258afcae65a496e989c49f3
a-clowes <clowes.alison@gmail.com>
2021-05-01 11:06:27 -0600

    Initial test info for QA

---
+++ __tests__/question.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '../utils/test-utils.jsx';
+import QuestionContext from '../client/src/components/question/QuestionContext.jsx';
+
+describe('QA tests', () => {
+  const questionMeta = {
+
+  };
+});
