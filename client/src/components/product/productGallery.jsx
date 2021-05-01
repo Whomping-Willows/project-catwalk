e851e4abb6819c0a9c42d4331c4aab1185e9fb42
Ben Lambert <kalstarn@gmail.com>
2021-05-01 11:17:39 -0600

    post merge commit

--- client/src/components/product/productGallery.jsx
+++ client/src/components/product/productGallery.jsx
@@ -11,7 +11,6 @@ const ProductGallery = (props) => {
   // console.log(styleData);
   // console.log(props.selectedStyle);
 
-<<<<<<< HEAD
   useEffect(() => {
     styleData.map((data) => {
       if (data.style_id === props.selectedStyle) {
@@ -27,26 +26,6 @@ const ProductGallery = (props) => {
       }
     });
   }, [styleData, props.selectedStyle]);
-=======
-  const mainImage = () => (
-    <img
-      id="mainImage"
-      alt={styleData[0].name}
-      src={styleData[0].photos[0].url}
-    />
-  );
-
-  const sideGallery = images[0].map((data, key) => (
-    <div className="sideImgDiv">
-      <img
-        key={`photo${key}`}
-        className="sideImage"
-        alt="Not found"
-        src={data.thumbnail_url}
-      />
-    </div>
-  ));
->>>>>>> 6faf9e95e3f2ff99f846d1953a0d58bff26e63ef
 
   return (
     <div className="productGallery">
