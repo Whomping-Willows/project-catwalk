3b2967a7c15c2544c0db50222096b0f5edc7fbad
a-clowes <70968591+a-clowes@users.noreply.github.com>
2021-05-04 10:18:29 -0600

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,3 @@
 // const GITHUB_API_KEY = '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';
 
-// AC token
-const GITHUB_API_KEY = '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';
 
-module.exports = GITHUB_API_KEY;
